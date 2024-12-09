9f0358a1e749c5b9abe7f80a2a54e9d4151eb1a1
BanaCubed <162780442+BanaCubed@users.noreply.github.com>
2024-12-10 12:33:16 +1300

    optimised scroll thingy

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ let index = Math.round((gameBoxes.scrollTop/gameBoxes.clientHeight)/1.1652323580
 markers.children[index].classList.add('activeMarker');
 
 gameBoxes.addEventListener("scroll", (event) => {
-    if (index != Math.round((gameBoxes.scrollTop/gameBoxes.clientHeight)/1.1652323580034423)) {
-        index = Math.round((gameBoxes.scrollTop/gameBoxes.clientHeight)/1.1652323580034423);
+    let newIndex = Math.round((gameBoxes.scrollTop/gameBoxes.clientHeight)/1.1652323580034423);
+    if (index != newIndex) {
+        index = newIndex;
         for (let i = 0; i < markers.children.length; i++) {
             const element = markers.children[i];
             element.classList.remove('activeMarker');
